8ef64b85ba709e462f65989a2a774317c2cbcf0f
Jackson Owens <jackson@chain.com>
2017-05-06 15:53:31 -0400

    remove cli dependency

--- main.go
+++ main.go
@@ -8,52 +8,35 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/codegangsta/cli"
 	"github.com/jbowens/request-catcher/catcher"
 )
 
 func main() {
-	cliApp := cli.NewApp()
-	cliApp.Name = "Request Catcher"
-	cliApp.Usage = "The Request Catcher web server"
-
-	cliApp.Commands = []cli.Command{
-		{
-			Name:      "start",
-			ShortName: "s",
-			Usage:     "Start the Request Catcher web server",
-			Action: func(c *cli.Context) {
-				args := c.Args()
-				if len(args) < 1 {
-					fmt.Println("Must provide config filename")
-					os.Exit(1)
-				}
-				configFilename := args.First()
-
-				config, err := catcher.LoadConfiguration(configFilename)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error loading configuration: %v\n", err)
-					os.Exit(1)
-				}
-
-				fullHost := config.Host + ":" + strconv.Itoa(config.Port)
-				handler := catcher.NewCatcher(config)
-				server := http.Server{
-					ReadTimeout:  5 * time.Second,
-					WriteTimeout: 10 * time.Second,
-					IdleTimeout:  120 * time.Second,
-					TLSNextProto: map[string]func(*http.Server, *tls.Conn, http.Handler){},
-					Addr:         fullHost,
-					Handler:      handler,
-				}
-
-				err = server.ListenAndServe()
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error listening on %s: %s\n", fullHost, err)
-					os.Exit(1)
-				}
-			},
-		},
+	if len(os.Args) < 2 {
+		fatalf("Usage: request-catcher <config-filename>\n")
 	}
-	cliApp.Run(os.Args)
+	config, err := catcher.LoadConfiguration(os.Args[1])
+	if err != nil {
+		fatalf("error loading configuration file: %s\n", err)
+	}
+
+	fullHost := config.Host + ":" + strconv.Itoa(config.Port)
+	handler := catcher.NewCatcher(config)
+	server := http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSNextProto: map[string]func(*http.Server, *tls.Conn, http.Handler){},
+		Addr:         fullHost,
+		Handler:      handler,
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		fatalf("error listening on %s: %s\n", fullHost, err)
+	}
+}
+
+func fatalf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+	os.Exit(1)
 }
