b96ce2cc5f00b82f9a52f7dc0808e655ab881365
Jackson Owens <jackson_owens@alumni.brown.edu>
2017-11-16 22:27:13 -0800

    Revert "remove cli dependency"
    This reverts commit c86afdcdd5395c69805610fb0edf8f42ad8345e2.

--- main.go
+++ main.go
@@ -12,57 +12,74 @@ import (
 
 	"golang.org/x/crypto/acme/autocert"
 
+	"github.com/codegangsta/cli"
 	"github.com/jbowens/request-catcher/catcher"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fatalf("Usage: request-catcher <config-filename>\n")
-	}
-	config, err := catcher.LoadConfiguration(os.Args[1])
-	if err != nil {
-		fatalf("error loading configuration file: %s\n", err)
-	}
+	cliApp := cli.NewApp()
+	cliApp.Name = "Request Catcher"
+	cliApp.Usage = "The Request Catcher web server"
 
-	handler := catcher.NewCatcher(config)
-	srv := http.Server{
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-		TLSNextProto: map[string]func(*http.Server, *tls.Conn, http.Handler){},
-		Addr:         config.ListenAddress(),
-		Handler:      handler,
-	}
+	cliApp.Commands = []cli.Command{
+		{
+			Name:      "start",
+			ShortName: "s",
+			Usage:     "Start the Request Catcher web server",
+			Action: func(c *cli.Context) {
+				args := c.Args()
+				if len(args) < 1 {
+					fmt.Println("Must provide config filename")
+					os.Exit(1)
+				}
+				configFilename := args.First()
 
-	// If there's no Let's Encrypt configuration, just launch
-	// without TLS.
-	if config.LetsEncrypt == nil {
-		err = srv.ListenAndServe()
-		if err != nil {
-			fatalf("error listening on %s: %s\n", config.ListenAddress(), err)
-		}
-		return
-	}
+				config, err := catcher.LoadConfiguration(configFilename)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error loading configuration: %v\n", err)
+					os.Exit(1)
+				}
 
-	// Setup an autocert configuration to dynamically acquire
-	// certificates for requested domains.
-	m := autocert.Manager{
-		Prompt:      autocert.AcceptTOS,
-		Cache:       autocert.DirCache(config.LetsEncrypt.CertsDirectory),
-		HostPolicy:  subdomainsHostPolicy(config.RootHost),
-		RenewBefore: 24 * time.Hour,
-	}
-	srv.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
+				handler := catcher.NewCatcher(config)
+				srv := http.Server{
+					ReadTimeout:  5 * time.Second,
+					WriteTimeout: 10 * time.Second,
+					IdleTimeout:  120 * time.Second,
+					TLSNextProto: map[string]func(*http.Server, *tls.Conn, http.Handler){},
+					Addr:         config.ListenAddress(),
+					Handler:      handler,
+				}
 
-	err = srv.ListenAndServeTLS("", "")
-	if err != nil {
-		fatalf("error listening on %s: %s\n", config.ListenAddress(), err)
-	}
-}
+				// If there's no Let's Encrypt configuration, just launch
+				// without TLS.
+				if config.LetsEncrypt == nil {
+					err = srv.ListenAndServe()
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "Error listening on %s: %s\n", config.ListenAddress(), err)
+						os.Exit(1)
+					}
+					return
+				}
 
-func fatalf(format string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, args...)
-	os.Exit(1)
+				// Setup an autocert configuration to dynamically acquire
+				// certificates for requested domains.
+				m := autocert.Manager{
+					Prompt:      autocert.AcceptTOS,
+					Cache:       autocert.DirCache(config.LetsEncrypt.CertsDirectory),
+					HostPolicy:  subdomainsHostPolicy(config.RootHost),
+					RenewBefore: 24 * time.Hour,
+				}
+				srv.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
+
+				err = srv.ListenAndServeTLS("", "")
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error listening on %s: %s\n", config.ListenAddress(), err)
+					os.Exit(1)
+				}
+			},
+		},
+	}
+	cliApp.Run(os.Args)
 }
 
 func subdomainsHostPolicy(rootHost string) autocert.HostPolicy {
