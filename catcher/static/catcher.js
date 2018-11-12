46a99a3867421988275330ba41c047287d579f7c
Jackson Owens <jackson_owens@alumni.brown.edu>
2018-11-11 23:01:45 -0800

    catcher: redesign catcher page

--- catcher/static/catcher.js
+++ catcher/static/catcher.js
@@ -5,7 +5,7 @@ window.catcher.connect = function() {
         conn = new WebSocket("wss://" + window.location.host + "/init-client");
         conn.onclose = function(evt) {
             console.log("connection closed", evt);
-            // Reconnet after a pause.
+            // Reconnect after a pause.
             setTimeout(window.catcher.connect, 1000);
         };
         conn.onmessage = function(evt) {
@@ -17,112 +17,37 @@ window.catcher.connect = function() {
     }
 };
 
-var td = function(tr, value, className) {
-  var cell = document.createElement('td');
-  $(cell).text(value);
-  if (className) {
-    $(cell).addClass(className);
-  }
-  $(tr).append(cell);
-};
-
-var makeDateTime = function(time) {
-  var dateDiv = document.createElement('div');
-  var timeDiv = document.createElement('div');
-  dateDiv.className = 'date';
-  timeDiv.className = 'time';
-  $(dateDiv).text(window.catcher.formatDate(time));
-  $(timeDiv).text(window.catcher.formatTime(time));
-  var dateTime = $('<div class="timestamp"></div>');
-  dateTime.append(dateDiv);
-  dateTime.append(timeDiv);
-  return dateTime;
-};
-
-var makeDiv = function(text, className) {
-  var div = document.createElement('div');
-  $(div).text(text);
-  $(div).addClass(className);
-  return div;
-};
-
 window.catcher.insertRequest = function(req) {
   console.log(req);
   var time = new Date(req.time);
-  var tr = document.createElement('tr');
-  $(tr).data('r', req);
 
-  var mainTd = document.createElement('td');
-  $(mainTd).addClass('general-info');
-  var methodAndPath = makeDiv(req.method + ' ' + req.path, 'method-and-path');
-  var dateTime = makeDateTime(time);
+  var mainDiv = document.createElement('div');
+  $(mainDiv).addClass('request');
+  $(mainDiv).data('r', req);
 
-  var a = document.createElement('a');
-  $(a).addClass('show-raw');
-  $(a).text('raw http request');
-  $(a).attr('href', '#');
-  var optionsDiv = document.createElement('div');
-  $(optionsDiv).append(a);
-  $(optionsDiv).addClass('options');
+  var pre = document.createElement('pre');
+  $(pre).text(req.raw_request);
 
-  $(mainTd).append(methodAndPath, dateTime, optionsDiv);
-  tr.appendChild(mainTd);
+  var metadata = document.createElement('div');
+  $(metadata).addClass('metadata');
+  $(metadata).text("Received at " + window.catcher.formatDate(time));
 
-  var bodyTd = document.createElement('td');
-  $(bodyTd).addClass('body');
-  var code = document.createElement('code');
-  $(code).text(req.body);
-  bodyTd.appendChild(code);
-  tr.appendChild(bodyTd);
+  $(mainDiv).append(pre, metadata);
 
-  if (req.headers['Content-Type'] &&
-      req.headers['Content-Type'][0] === "application/json") {
-    $(tr).find('.body code').each(function(i, block) {
-      hljs.highlightBlock(block);
-    });
-  }
-
-  window.catcher.addListeners(tr);
-  window.catcher.heading.after(tr);
-  if (!window.catcher.table.is(':visible')) {
+  window.catcher.requests.prepend(mainDiv);
+  if (!window.catcher.requests.is(':visible')) {
     window.catcher.noRequests.hide();
-    window.catcher.table.show();
+    window.catcher.requests.show();
   }
 };
 
 window.catcher.formatDate = function(date) {
-  return moment(date).format("dddd, MMMM Do YYYY");
-};
-
-window.catcher.formatTime = function(date) {
-  return moment(date).format("h:mm:ss a");
-};
-
-window.catcher.addListeners = function(row) {
-  $(row).find('.show-raw').click(window.catcher.showRaw);
-};
-
-window.catcher.showRaw = function(evt) {
-  evt.preventDefault();
-  var req = $(evt.currentTarget).closest('tr').data('r');
-  window.catcher.rawContent.text(req.raw_request);
-  window.catcher.rawContent.each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
-  window.catcher.rawPopup.show();
+  return moment(date).format();
 };
 
 $(document).ready(function() {
-  window.catcher.table = $('table#caught-requests');
-  window.catcher.heading = $('table#caught-requests tr.heading');
+  window.catcher.requests = $('#requests');
   window.catcher.noRequests = $('#no-requests');
-  window.catcher.rawPopup = $('#raw-popup');
-  window.catcher.rawContent = $('#raw-popup #raw-content');
-
-  $('#raw-popup .close-popup').click(function(e) {
-    window.catcher.rawPopup.hide();
-  });
-
   $('#hostname').text(window.location.host);
 
   window.catcher.connect();
