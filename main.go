c1d0515caac72775d5f462ca6e12a1acf7519426
Jackson Owens <jackson_owens@alumni.brown.edu>
2018-11-12 18:57:58 -0800

    main.go: relax to TLS 1.1+

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 			tls.CurveP256,
 			tls.X25519,
 		},
-		MinVersion: tls.VersionTLS12,
+		MinVersion: tls.VersionTLS11,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
