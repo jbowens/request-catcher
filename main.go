be5ba6265f80092d537017f732dad6b26ae62a0c
Jackson Owens <jackson_owens@alumni.brown.edu>
2018-11-12 19:07:20 -0800

    main.go: relax TLS config
    We don't want a client's shitty TLS support to cause us to be unable to
    accept requests. I think we want to be very permissive, but ideally
    _surface_ information about the shitty TLS support to the user in the
    UI.

--- main.go
+++ main.go
@@ -22,24 +22,7 @@ func main() {
 		fatalf("error loading configuration file: %s\n", err)
 	}
 
-	// tls.Config configuration taken from
-	// https://blog.cloudflare.com/exposing-go-on-the-internet/#cryptotls
-	tlsconf := &tls.Config{
-		PreferServerCipherSuites: true,
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP256,
-			tls.X25519,
-		},
-		MinVersion: tls.VersionTLS11,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
-	}
+	tlsconf := &tls.Config{MinVersion: tls.VersionTLS10}
 
 	// Start a http server to redirect http traffic to https
 	srv := &http.Server{
